de29e16ec7730203dc29105ec6b829d1dfef185f
Hong0249 <77378104+Hong0249@users.noreply.github.com>
2022-05-02 01:46:03 +0800

    Add files via upload
    modified files

--- user.js
+++ user.js
@@ -5,21 +5,30 @@ class User {
 		users = await conn.db("my-database-name").collection("users")
 	}
 
-	static async register(username, password) {
-		// TODO: Check if username exists
+	static async Register(name, age, gender, room_number, contact_number, mysj_status, email, password) {
+		// TODO: Register name, age, gender, room_number, contact_number, mysj_status, email exists
 
 		// TODO: Hash password
 
-		// TODO: Save user to database
+		// TODO: Save BookingandReservation to database
 	}
 
-	static async login(username, password) {
-		// TODO: Check if username exists
+	static async BookingandReservation(name, email, password, time_slot, number_of_visitors) {
+		// TODO: Check if name, email, time_slot, number_of_viitors exists
+
+		// TODO: Hash password
+
+		// TODO: Save BookingandReservation to database
+        return
+	}
+
+	static async Facilities_info(facilities_manager_name, location, max_num_of_visitor, facilities_manager_contact_number, email, password) {
+		// TODO: Check if facilitiesmanagername, location, max_num_of_visitor, facilities_manager_contact_number, email exists
 
 		// TODO: Validate password
 
-		// TODO: Return user object
-		return;
+		// TODO: Save Facilities_info to database
+        return
 	}
 }
 
--- user.test.js
+++ user.test.js
@@ -15,28 +15,43 @@ describe("User Account", () => {
 		await client.close();
 	})
 
-	test("New user registration", async () => {
-		const res = await User.register("test", "test")
-		expect().toBe()
+	test("Register the visitor account", async () => {
+		const res = await User.register("name", "age", "gender", "room_number", "contact_number", "mysj_status", "email", "password")
+		expect(res).toBeTruthy()
 	})
 
-	test("Duplicate username", async () => {
-		const res = await User.register("test", "test")
-		expect().toBe()
+	test("Doing the booking and reservation", async () => {
+		const res = await User.BookingandReservation("name", "email", "password", "time_slot", "number_of_visitors")
+		expect(res).toBeTruthy()
 	})
 
-	test("User login invalid username", async () => {
-		const res = await User.login("test", "test")
-		expect().toBe()
+    test("Doing the facilities info", async () => {
+		const res = await User.BookingandReservation("facilities_manager_name", "location", "max_num_of_visitor", "facilities_manager_contact_number", "email", "password")
+		expect(res).toBeTruthy()
+	})
+
+    test("User login invalid name, email", async () => {
+		const res = await User.login("name", "email")
+		expect(res).toBeFalsy()
+        try{
+            await User.login()
+        } catch(e){
+            expext(e).toMatch("invalid name and email")
+        }
 	})
 
 	test("User login invalid password", async () => {
-		const res = await User.login("test", "test")
-		expect().toBe()
+		const res = await User.login("password")
+		expect(res).toBeFalsy()
+        try{
+            await User.login()
+        } catch(e){
+            expext(e).toMatch("invalid password")
+        }
 	})
 
 	test("User login successfully", async () => {
-		const res = await User.login("test", "test")
+		const res = await User.login("name", "email", "password")
 		expect(res).toBe(true)
 	})
 });
