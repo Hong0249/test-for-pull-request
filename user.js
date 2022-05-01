f3069b4c28d7d08da36e839a84f52497d3ef8a21
YAK <yakxuanhong11300@gmail.com>
2022-05-02 01:43:22 +0800

    new code!

---
+++ user.js
@@ -0,0 +1,35 @@
+let users;
+
+class User {
+	static async injectDB(conn) {
+		users = await conn.db("my-database-name").collection("users")
+	}
+
+	static async Register(name, age, gender, room_number, contact_number, mysj_status, email, password) {
+		// TODO: Register name, age, gender, room_number, contact_number, mysj_status, email exists
+
+		// TODO: Hash password
+
+		// TODO: Save BookingandReservation to database
+	}
+
+	static async BookingandReservation(name, email, password, time_slot, number_of_visitors) {
+		// TODO: Check if name, email, time_slot, number_of_viitors exists
+
+		// TODO: Hash password
+
+		// TODO: Save BookingandReservation to database
+        return
+	}
+
+	static async Facilities_info(facilities_manager_name, location, max_num_of_visitor, facilities_manager_contact_number, email, password) {
+		// TODO: Check if facilitiesmanagername, location, max_num_of_visitor, facilities_manager_contact_number, email exists
+
+		// TODO: Validate password
+
+		// TODO: Save Facilities_info to database
+        return
+	}
+}
+
+module.exports = User;
---
+++ user.test.js
@@ -0,0 +1,57 @@
+const MongoClient = require("mongodb").MongoClient;
+const User = require("./user")
+
+describe("User Account", () => {
+	let client;
+	beforeAll(async () => {
+		client = await MongoClient.connect(
+			"my-mongodb+srv-connection-string",
+			{ useNewUrlParser: true },
+		);
+		User.injectDB(client);
+	})
+
+	afterAll(async () => {
+		await client.close();
+	})
+
+	test("Register the visitor account", async () => {
+		const res = await User.register("name", "age", "gender", "room_number", "contact_number", "mysj_status", "email", "password")
+		expect(res).toBeTruthy()
+	})
+
+	test("Doing the booking and reservation", async () => {
+		const res = await User.BookingandReservation("name", "email", "password", "time_slot", "number_of_visitors")
+		expect(res).toBeTruthy()
+	})
+
+    test("Doing the facilities info", async () => {
+		const res = await User.BookingandReservation("facilities_manager_name", "location", "max_num_of_visitor", "facilities_manager_contact_number", "email", "password")
+		expect(res).toBeTruthy()
+	})
+
+    test("User login invalid name, email", async () => {
+		const res = await User.login("name", "email")
+		expect(res).toBeFalsy()
+        try{
+            await User.login()
+        } catch(e){
+            expext(e).toMatch("invalid name and email")
+        }
+	})
+
+	test("User login invalid password", async () => {
+		const res = await User.login("password")
+		expect(res).toBeFalsy()
+        try{
+            await User.login()
+        } catch(e){
+            expext(e).toMatch("invalid password")
+        }
+	})
+
+	test("User login successfully", async () => {
+		const res = await User.login("name", "email", "password")
+		expect(res).toBe(true)
+	})
+});
