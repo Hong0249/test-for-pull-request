10cd3f0902335d67a4b44813d7f795bd18161884
psuedoco <104721464+psuedoco@users.noreply.github.com>
2022-05-04 19:02:59 +0800

    Update user.js

--- user.js
+++ user.js
@@ -2,61 +2,34 @@ let users;
 
 class User {
 	static async injectDB(conn) {
-		users = await conn.db("vms").collection("visitor")
+		users = await conn.db("my-database-name").collection("users")
 	}
 
-	static async Register(name, ic_no, email, hp, mysj_status) {
-		// TODO: Check name, email exists
-        let result = await conn.db('vms').collection('visitor').find(
-            {email: email}).toArray();
-        if(result.length > 0){
-            return false;
-        }
+	static async Register(name, age, gender, room_number, contact_number, mysj_status, email, password) {
+		// TODO: Register name, age, gender, room_number, contact_number, mysj_status, email exists
 
-		// TODO: Save new user to database
-        let result2 = await conn.db('vms').collection('visitor').insertOne({
-            name: name,
-            ic_no: ic_no,
-            email: email,
-            hp: hp,
-            mysj_status: mysj_status
-        });
+		// TODO: Hash password
+
+		// TODO: Save BookingandReservation to database
 	}
 
-	static async BookingandReservation(facilities_id, visitor_id, time_slot) {
-		// TODO: Check if current booking is full
-        let result = await conn.db('vms').collection('booking_request').find(
-            {facilities_id: facilities_id, time_slot: time_slot}).countDocuments();
+	static async BookingandReservation(name, email, password, time_slot, number_of_visitors) {
+		// TODO: Check if name, email, time_slot, number_of_viitors exists
 
-        let facilities = await conn.db('vms').collection('facilities').find(
-            {facilities_id: facilities_id}).toArray();
-        if(result >= facilities.max_num_of_visitor){
-            return false;
-        }
+		// TODO: Hash password
 
-        // TODO: Check if duplicate booking
-        let result2 = await conn.db('vms').collection('booking_request').find(
-            {facilities_id: facilities_id, time_slot: time_slot, visitor_id: visitor_id}).toArray();
-        if(result2.length > 0){
-            return false;
-        }
-
-		// TODO: Save booking request to database
-        await conn.db('vms').collection('booking_request').insertOne({
-            facilities_id: facilities_id,
-            visitor_id: visitor_id,
-            time_slot: time_slot
-        });
+		// TODO: Save BookingandReservation to database
+        return
 	}
 
-	static async Facilities_info(facilities_name) {
-		// TODO: Query facilities_name
-        let result = await conn.db('vms').collection('facilities').find(
-            {facilities_name: facilities_name}).toArray();
-        if(result.length > 0){
-            return result;
-        }
-    }
+	static async Facilities_info(facilities_manager_name, location, max_num_of_visitor, facilities_manager_contact_number, email, password) {
+		// TODO: Check if facilitiesmanagername, location, max_num_of_visitor, facilities_manager_contact_number, email exists
+
+		// TODO: Validate password
+
+		// TODO: Save Facilities_info to database
+        return
+	}
 }
 
 module.exports = User;
